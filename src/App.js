6973be16ebdd58627a7951e8ffec2e034e148466
Dara Olayebi <dara.olayebi@gmail.com>
2021-03-24 18:19:54 +0100

    fixed mobile typed issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./assets/styles/App.scss";
-import React, {Component, Fragment, Suspense, lazy} from "react";
+import React, {Fragment, Suspense, lazy} from "react";
 import {Route, Switch} from "react-router-dom";
 
 import AppHeader from "./components/AppHeader";
@@ -10,32 +10,23 @@ const About = lazy(() => import("./views/About"));
 const SubmitStory = lazy(() => import("./views/SubmitStory"));
 const AllStories = lazy(() => import("./views/AllStories"));
 
-class App extends Component {
-	// componentDidUpdate(prevProps) {
-	// 	console.log("ROUTE CHANGED");
-	// 	if (this.props.location !== prevProps.location) {
-	// 		console.log("ROUTE CHANGED");
-	// 		this.onRouteChanged();
-	// 	}
-	// }
-
-	render() {
-		return (
-			<Fragment>
-				<AppHeader />
-				<Switch>
-					<Suspense fallback={<div style={{color: "red", fontSize: "100px"}}>Loading...</div>}>
-						<Route path="/about-project" component={About}></Route>
-						<Route path="/submit-story" component={SubmitStory}></Route>
-						<Route path="/explore-stories" component={AllStories}></Route>
-						<Route exact path="/" component={Home}></Route>
-						{/* <Route render={() => <h1>404: page not found</h1>} /> */}
-					</Suspense>
-				</Switch>
-				<AppNav />
-			</Fragment>
-		);
-	}
-}
+const App = () => {
+	return (
+		<Fragment>
+			<AppHeader />
+			<Switch>
+				<Suspense fallback={<div style={{color: "red", fontSize: "100px"}}>Loading...</div>}>
+					<Route path="/about-project" component={About}></Route>
+					<Route path="/submit-story" component={SubmitStory}></Route>
+					<Route path="/explore-stories" component={AllStories}></Route>
+					<Route exact path="/" component={Home}></Route>
+					{/* <Route render={() => <h1>404: page not found</h1>} /> */}
+				</Suspense>
+			</Switch>
+			<AppNav />
+			{/*  */}
+		</Fragment>
+	);
+};
 
 export default App;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,15 +6,28 @@ class Home extends Component {
 	state = {
 		slideshowVisible: false,
 		slideshow: "",
+		width: 0,
+		height: 0,
+	};
+
+	updateWindowDimensions = () => {
+		this.setState({width: window.innerWidth, height: window.innerHeight});
 	};
 
 	componentDidMount() {
-		setTimeout(() => {
+		this.timeout = setTimeout(() => {
 			let home = document.querySelector(".home");
 			this.setState({slideshowVisible: true});
 			if (home) home.classList.add("no-divider");
 		}, 2500);
 		document.body.classList.remove("show-menu");
+		this.updateWindowDimensions();
+		window.addEventListener("resize", this.updateWindowDimensions);
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.timeout);
+		window.removeEventListener("resize", this.updateWindowDimensions);
 	}
 
 	render() {
@@ -24,12 +37,19 @@ class Home extends Component {
 					<div className="intro-content">
 						{this.state.slideshowVisible && <img src={SlideshowGif} alt="Slideshow" className="slideshow-gif" />}
 						<h1 className="home-intro">
-							<Typed
-								strings={[
-									"Take a moment to share your 2020 story and discover what the rest of the world has been through",
-								]}
-								typeSpeed={40}
-							/>
+							{this.state.width <= 640 ? (
+								<span>
+									Take a moment to share your 2020 story and discover what the rest of the world has been
+									through
+								</span>
+							) : (
+								<Typed
+									strings={[
+										"Take a moment to share your 2020 story and discover what the rest of the world has been through",
+									]}
+									typeSpeed={40}
+								/>
+							)}
 						</h1>
 					</div>
 				</div>
