a6f1f990e58261beead72bd255a925b9dc38f38b
Dara Olayebi <dara.olayebi@gmail.com>
2024-02-03 23:01:08 +0000

    update meta description

--- src/resources/apiBaseUrl.js
+++ src/resources/apiBaseUrl.js
@@ -1,4 +1,6 @@
-const baseURL = "https://surviving-2020.herokuapp.com/api/v1";
+const isRunningLocally = false;
+const baseURL = isRunningLocally ? "http://localhost:3000/api/v1" :
+    "https://surviving-2020-d717e9f48057.herokuapp.com/api/v1";
 
 
 export default baseURL;
