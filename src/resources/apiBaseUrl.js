5345a5c3b376ad52d13258d45ad246917ad78cf4
Dara Olayebi <dara.olayebi@gmail.com>
2026-02-09 18:03:55 +0000

    Update API base URL for production environment

--- src/resources/apiBaseUrl.js
+++ src/resources/apiBaseUrl.js
@@ -1,6 +1,6 @@
 const isRunningLocally = false;
 const baseURL = isRunningLocally ? "http://localhost:3000/api/v1" :
-    "https://surviving-2020-d717e9f48057.herokuapp.com/api/v1";
+    "https://surviving-2020-api.onrender.com/api/v1";
 
 
 export default baseURL;
